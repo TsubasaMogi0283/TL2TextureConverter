1c2f3c3e05f83e8f43da12c5a002b748366c4109
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-10-01 03:33:35 +0900

    /の途中

--- TextureConverter/TextureConverter.cpp
+++ TextureConverter/TextureConverter.cpp
@@ -44,3 +44,42 @@ std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::str
 	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&string[0]), static_cast<int>(string.size()), &result[0], sizeNeeded);
 	return result;
 }
+
+void TextureConverter::SeparateFilePath(const std::wstring& filePath){
+	size_t pointPosition;
+	std::wstring exceptExt;
+
+
+	//「.」が出てキル最後の部分を探す
+	pointPosition = filePath.rfind('.');
+	if (pointPosition != std::wstring::npos) {
+		//区切り文字の後ろをファイル拡張子として保存
+		fileExt_ = filePath.substr(pointPosition + 1, filePath.size() - pointPosition - 1);
+		//区切り文字の前までを抜き出す
+		exceptExt = filePath.substr(0, pointPosition);
+	}
+	else {
+		fileExt_ = L"";
+		exceptExt = filePath;
+	}
+
+
+	//「\\」が出てくる最後の部分を探す
+	pointPosition = exceptExt.rfind('\\');
+	if (pointPosition != std::wstring::npos) {
+		//区切り文字の前までをディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pointPosition + 1);
+		//区切り文字の前までを抜き出す
+		fileName_ = exceptExt.substr(pointPosition + 1, exceptExt.size() - pointPosition - 1);
+		return;
+	}
+
+
+
+	//区切り文字「/」が出てくる最後の部分を探す
+	pointPosition = exceptExt.rfind('/');
+	if (pointPosition != std::wstring::npos) {
+
+	}
+
+}
--- TextureConverter/TextureConverter.h
+++ TextureConverter/TextureConverter.h
@@ -13,7 +13,7 @@ public:
 	/// <summary>
 	/// テクスチャをWICからDDSに変換する
 	/// </summary>
-	/// <param name="filePath"></param>
+	/// <param name="filePath">ファイルパス</param>
 	void ConvertTextureWICToDDS(const std::string& filePath);
 
 	/// <summary>
@@ -27,24 +27,37 @@ private:
 	/// <summary>
 	/// テクスチャファイルの読み込み
 	/// </summary>
-	/// <param name="filePath"></param>
+	/// <param name="filePath">ファイルパス</param>
 	void LoadWICTextureFromFile(const std::string& filePath);
 
 
 	/// <summary>
 	/// マルチバイト文字列をワイド文字列に変換
 	/// </summary>
-	/// <param name="string"></param>
+	/// <param name="string">返還前の文字列</param>
 	/// <returns></returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& string);
+
+
+	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
 
 
 private:
 	//画像の情報
 	DirectX::TexMetadata metadata_;
-
 	//画像イメージのコンテナ
 	DirectX::ScratchImage scratchImage_;
+
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 
 
 };
